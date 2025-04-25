5e1738f17c03c9b299ddd342d2464835217b54a2
Kadir Kobak <kadir.kobak@celsan.com>
2025-04-25 16:17:49 +0300

    Designs updated.

--- RapidApi_Currency/Form1.Designer.cs
+++ RapidApi_Currency/Form1.Designer.cs
@@ -37,6 +37,7 @@
             this.label4 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -131,11 +132,22 @@
             this.textBox2.Size = new System.Drawing.Size(162, 30);
             this.textBox2.TabIndex = 8;
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(228, 407);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 34);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Uygula";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 490);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label4);
@@ -163,6 +175,7 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
     }
 }
 

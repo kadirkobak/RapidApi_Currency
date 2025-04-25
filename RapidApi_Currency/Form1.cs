e7bdd4460390274afbfec263cb9dccd94f4cd139
Kadir Kobak <kadir.kobak@celsan.com>
2025-04-25 16:25:03 +0300

    JSON updated.

--- RapidApi_Currency/Form1.cs
+++ RapidApi_Currency/Form1.cs
@@ -4,9 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
 
 namespace RapidApi_Currency
 {
@@ -15,6 +17,29 @@ namespace RapidApi_Currency
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://currency-conversion-and-exchange-rates.p.rapidapi.com/timeseries?start_date=2019-01-01&end_date=2019-01-02&base=USD&symbols=EUR%2CGBP"),
+                Headers =
+    {
+        { "x-rapidapi-key", "ee46bfe5f8msh9847dc72b78f403p12245cjsn61d2010bb2e7" },
+        { "x-rapidapi-host", "currency-conversion-and-exchange-rates.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                //Console.WriteLine(body);
+                var json = JObject.Parse(body);
+                var value = json["result"].ToString();
+            }
         }
     }
 }
